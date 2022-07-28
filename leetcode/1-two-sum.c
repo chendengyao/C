97f63ab239cab89e0bc785992a20862cc0305719
chendengyao <1171077218@qq.com>
2022-07-28 11:06:42 +0800

    add leetcode question 1 two sum

---
+++ leetcode/1-two-sum.c
@@ -0,0 +1,42 @@
+
+/*
+给定一个整数数组 nums 和一个整数目标值 target，请你在该数组中找出 和为目标值 target  的那 两个 整数，并返回它们的数组下标。
+
+你可以假设每种输入只会对应一个答案。但是，数组中同一个元素在答案里不能重复出现。
+
+你可以按任意顺序返回答案。
+
+来源：力扣（LeetCode）
+链接：https://leetcode.cn/problems/two-sum
+著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+*/
+
+
+#include "stdio.h" // 包含输入输出标准库
+
+/**
+ * Note: The returned array must be malloced, assume caller calls free().
+ */
+int* twoSum(int* nums, int numsSize, int target, int* returnSize){
+    int* result = NULL;
+    for (int i = 0; i < numsSize; i++)
+    {
+        for (int j = i + 1; j < numsSize; j++)
+        {
+            if (nums[i] + nums[j] == target)
+            {
+                returnSize[0] = 2;
+                result=(int*)malloc(sizeof(int)*2);
+                result[0] = i;
+                result[1] = j;
+                return result;
+            }
+        } 
+    }
+    return result;
+}
+
+int main() {
+    printf("Hello World!"); // 输出字符串到控制台
+    return 0;
+}
